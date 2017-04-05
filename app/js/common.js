994df336b0474efeae27968499f9cd96f3fbb802
elnasim <archlich21@yandex.ru>
2017-04-05 12:56:16 +0300

    next

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,32 @@
 $(function() {
 
 	$(".toggle-menu").click(function() {
-  $(this).toggleClass("on");
-  $(".mobile-menu").slideToggle();
-  return false;
-});
+		$(this).toggleClass("on");
+		$(".mobile-menu").slideToggle();
+		return false;
+	});
+
+	//magnific-popup
+	$('.gallery-item').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+
+
+
 
 });
